771b94006c9ce554d15f8b4c87e6f93205b097e9
Oscar <juliangomes8090@gmail.com>
2025-05-23 20:26:32 -0500

    controller.js

--- BackEnd/Controles/estudiantescontroller.js
+++ BackEnd/Controles/estudiantescontroller.js
@@ -1,3 +1,28 @@
 // estudiantescontroller.js
+import * as router from "../routes/estudiantesRouter.js";
 
-let estudiantes = JSON.parse(localStorage.getItem("estudiantes")) || [];
+export async function agregarEstudianteController(dni, nombre, apellidos, email) {
+    if (!dni || !nombre || !apellidos || !email) {
+        return { exito: false, mensaje: "Todos los campos son obligatorios." };
+    }
+
+    const estudiante = { dni, nombre, apellidos, email };
+    return await router.agregarEstudiante(estudiante);
+}
+
+export async function buscarEstudianteController(dni) {
+    if (!dni || dni.length < 2) {
+        return { exito: false, mensaje: "Documento no válido." };
+    }
+
+    const encontrado = await router.buscarEstudiantePorDocumento(dni);
+    if (encontrado) {
+        return { exito: true, datos: encontrado };
+    } else {
+        return { exito: false, mensaje: "Estudiante no encontrado." };
+    }
+}
+
+export async function listarEstudiantesController() {
+    return await router.listarEstudiantes();
+}
